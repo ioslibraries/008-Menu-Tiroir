67b9c93da547e4b780b27c789f4bf9ae4c8c33ea
jayztemplier <templier.jeremy@gmail.com>
2012-04-05 19:28:03 +0200

    Initial Commit

---
+++ Deck/ILAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ILAppDelegate.h
+//  Deck
+//
+//  Created by jeremy Templier on 05/04/12.
+//  Copyright (c) 2012 particulier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ILAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Deck/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Deck
+//
+//  Created by jeremy Templier on 05/04/12.
+//  Copyright (c) 2012 particulier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ILAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ILAppDelegate class]));
+    }
+}
